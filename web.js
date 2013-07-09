6ef966073ebedd5df47561d803299b8ea4a03fd0
Ubuntu <psi.dexter@gmail.com>
2013-07-09 17:26:19 +0000

    modify web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
+var index = fs.readFileSync('index.html');
+var content = index.toString('utf8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
