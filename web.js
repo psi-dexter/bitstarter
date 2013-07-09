d2622dad9b930bae4511e9072fce2ac0a8dc9974
Ubuntu <psi.dexter@gmail.com>
2013-07-09 17:43:20 +0000

    add second content file

--- web.js
+++ web.js
@@ -2,11 +2,16 @@ var express = require('express');
 
 var fs = require('fs');
 var index = fs.readFileSync('index.html');
+var index2 = fs.readFileSync('index2.html');
 var content = index.toString('utf8');
+var content2 = index2.toString('utf8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(content);
+});
+app.get('/v2', function(request, response) {                                                                          
+  response.send(content2);                                                                                           
 });
 
 var port = process.env.PORT || 5000;
